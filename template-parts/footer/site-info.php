01e4d1e466005f612ffdd3ef76d115b26416e51a
William Bruno <wbrunom@gmail.com>
2017-02-25 12:15:52 -0300

    add: init project

---
+++ template-parts/footer/site-info.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Displays footer site info
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+<div class="site-info">
+	<a href="<?php echo esc_url( __( 'https://twitter.com/marigpoficial', 'twentyseventeen' ) ); ?>"><?php printf( __( '%s', 'twentyseventeen' ), 'Mariana Lidel' ); ?></a>
+</div><!-- .site-info -->
